39b9f6a67d74a6534dfc50c4b80931214c4d27ba
salsanfilippo <sal@sanfilippo.ws>
2014-06-11 15:34:46 -0700

    2 way binding

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,6 +2,12 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
+        $scope.saveEvent = function(event) {
+            window.alert('Event "' + event.name + '" saved.');
+        };
 
+        $scope.cancelEvent = function() {
+            window.location = '/EventDetails.html';
+        }
     }
 )
