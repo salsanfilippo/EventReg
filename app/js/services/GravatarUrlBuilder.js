6d735bf2c70a5bdd6b7c428ad95424c9fb835274
salsanfilippo <sal@sanfilippo.ws>
2014-06-12 11:28:28 -0700

    Tweak layout of EditProfile

--- app/js/services/GravatarUrlBuilder.js
+++ app/js/services/GravatarUrlBuilder.js
@@ -3,7 +3,7 @@
 eventsApp.factory('gravatarUrlBuilder', function() {
     return {
         buildGravatarUrl: function(email) {
-            var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
+            var defaultGravatarUrl = "/img/profile.jpg";
 
             var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if (!regex.test(email))
