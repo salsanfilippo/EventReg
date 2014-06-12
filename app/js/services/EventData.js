5351caeed5030c1a07f024a022ab112b3bc8bc8f
salsanfilippo <sal@sanfilippo.ws>
2014-06-12 10:59:02 -0700

    Started Edit Profile

---
+++ app/js/services/EventData.js
@@ -0,0 +1,44 @@
+'use strict';
+
+eventsApp.factory('eventData', function () {
+    return {
+        event: {
+            name: 'Angular Boot Camp',
+            date: '2013-01-01',
+            time: '2013-01-01T10:30:00',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions:
+            [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for Fun and Profit',
+                    creatorName: 'John Doe',
+                    duration: 2,
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: 4,
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of colleagues.',
+                    upVoteCount: 0
+                }
+            ]
+        }
+    };
+});
